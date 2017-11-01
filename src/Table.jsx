ca61dc7b7d5ec4043ae90962b6029f84017d1dbe
姚嘉松 <yaojiasong@gooagoo.com>
2017-11-01 17:39:12 +0800

    组件转移 无线 UI 组件 table

---
+++ src/Table.jsx
@@ -0,0 +1,40 @@
+import '../style';
+import React from 'react';
+import assign from 'object-assign';
+import RcTable from 'rc-table';
+
+class Table extends React.Component {
+
+
+  render() {
+    const { prefixCls, columns, dataSource, direction, scrollX, titleFixed } = this.props;
+
+    const newProps = assign({}, this.props, { data: dataSource });
+
+    let table;
+    // 默认纵向
+    if (!direction || direction === 'vertical') {
+      if (titleFixed) {
+        table = <RcTable {...newProps} columns={columns} scroll={{ x: true }} showHeader={false} />;
+      } else {
+        table = <RcTable {...newProps} columns={columns} scroll={{ x: scrollX }} />;
+      }
+    // 横向
+    } else if (direction === 'horizon') {
+      columns[0].className = `${prefixCls}-horizonTitle`;
+      table = <RcTable {...newProps} columns={columns} showHeader={false} scroll={{ x: scrollX }} />;
+    // 混合
+    } else if (direction === 'mix') {
+      columns[0].className = `${prefixCls}-horizonTitle`;
+      table = <RcTable {...newProps} columns={columns} scroll={{ x: scrollX }} />;
+    }
+
+    return table;
+  }
+}
+Table.defaultProps = {
+  dataSource: [],
+  prefixCls: 'am-table',
+};
+Table.displayName = "Table";
+module.exports=Table;
